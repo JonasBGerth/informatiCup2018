2106a3aafc73c9990c2e052f820f3d5542a0b88b
Ole Scheller <olescheller87@gmail.com>
2017-11-26 14:17:43 +0100

    Add initializer

--- process_data.py
+++ process_data.py
@@ -14,8 +14,12 @@ class InformatiCup2018(object):
         self.gas_stations_dfs = {}
         self.gas_stations_models = {}
 
+    def init_all(self):
+        self.read_all_gas_stations()
+        self.create_models_for_all_gas_stations()
+
     def read_gas_station_data(self, gas_station_id: int):
-        """"""
+        print(f"Reading data for gas station {gas_station_id}.")
         data = pd.read_csv(os.path.join(self.gas_stations_dir, f'{gas_station_id}.csv'),
                            sep=';', header=None,
                            names=['ds', 'y'])
@@ -26,21 +30,25 @@ class InformatiCup2018(object):
         return data
 
     def read_all_gas_stations(self):
-        print(os.listdir(self.data_dir + os.path.join('Eingabedaten', 'Benzinpreise')))
         for file in os.listdir(self.gas_stations_dir):
             m = re.search(r'(.*)[.]', file)
             id = int(m.group(1))
-            print(id)
-
             self.gas_stations_dfs[id] = self.read_gas_station_data(id)
 
     def create_model_for_gas_station(self, gas_station_id: int):
+        print(f"Creating model for gas station {gas_station_id}.")
         m = Prophet()
         # TODO: Add holidays to each gas station
 
         m.fit(self.gas_stations_dfs[gas_station_id])
         self.gas_stations_models[gas_station_id] = m
         return m
+
+    def create_models_for_all_gas_stations(self):
+        for file in os.listdir(self.gas_stations_dir):
+            m = re.search(r'(.*)[.]', file)
+            id = int(m.group(1))
+            self.gas_stations_models[id] = self.create_model_for_gas_station(id)
 
     def predict_prices(self, gas_station_id: int, dates: List[str]):
         predict_date = pd.DataFrame()
@@ -51,16 +59,10 @@ class InformatiCup2018(object):
 
 if __name__ == '__main__':
     ic = InformatiCup2018(data_dir="/Users/ole/InformatiCup2018/")
-
-    data = ic.read_gas_station_data(1)
-    print(data.tail())
-
-    ic.create_model_for_gas_station(1)
-    print(ic.predict_prices(1, [
-        '2017-09-21 11:54:16',
-        '2017-09-21 12:00:00',
-        '2017-09-21 13:39:06',
-        '2017-09-21 15:13:05',
-        '2017-09-21 15:47:05',
-        '2017-09-21 21:03:06'
-    ]))
+    # data = ic.read_gas_station_data(1)
+    # predict_dates = list(map(str, data['ds'].tolist()[-5:]))
+    # print(data.tail())
+    # print(predict_dates)
+    # ic.create_model_for_gas_station(1)
+    # print(ic.predict_prices(1, predict_dates))
+    ic.init_all()
