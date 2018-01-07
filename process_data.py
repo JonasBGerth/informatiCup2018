db58c349ae53fe8fc68ff9a4b357a92303ae7ed4
Ole Scheller <olescheller87@gmail.com>
2018-01-07 18:31:43 +0100

    Implement tanking fuel amounts
    + Clean up

--- process_data.py
+++ process_data.py
@@ -14,11 +14,13 @@ def dist(a, b):
     return 6378.388 * math.acos((math.sin(math.radians(a[1])) * math.sin(math.radians(b[1]))) + math.cos(math.radians(a[1])) * math.cos(
         math.radians(b[1])) * math.cos(math.radians(b[0]) - math.radians(a[0])))
 
+
 class InformatiCup2018(object):
     def __init__(self, data_dir):
         self.data_dir = data_dir
         self.gas_stations_data_dir = data_dir + os.path.join('Eingabedaten', 'Benzinpreise')
         self.gas_stations_dir = data_dir + os.path.join('Eingabedaten', 'Tankstellen.csv')
+        self.routes = data_dir + os.path.join('Eingabedaten', 'Fahrzeugrouten')
         self.gas_stations = {}
         self.gas_stations_dfs = {}
         self.gas_stations_models = {}
@@ -28,7 +30,7 @@ class InformatiCup2018(object):
         self.create_models_for_all_gas_stations()
 
     def init_gas_stations(self):
-        self.gas_stations = pd.read_csv(self.gas_stations_dir, sep=';', header=None, index_col = 'id',
+        self.gas_stations = pd.read_csv(self.gas_stations_dir, sep=';', header=None, index_col='id',
                                         names=['id', 'name', 'brand', 'street', 'house_no', 'plz', 'town', 'Long',
                                                'Lat'])
 
@@ -65,15 +67,26 @@ class InformatiCup2018(object):
             self.gas_stations_models[id] = self.create_model_for_gas_station(id)
 
     def predict_prices(self, gas_station_id: int, dates: List[str]):
+        """Predict a price for a gas station at specific dates/times."""
         predict_date = pd.DataFrame()
         predict_date['ds'] = pd.to_datetime(dates)
         forecast = self.gas_stations_models[gas_station_id].predict(predict_date)
         return forecast[['ds', 'yhat']]
 
-    def predict_route_prices(self, file_name):
+    def get_all_route_files(self):
+        """Return a list of all route files inside the `Eingabedaten/Fahrzeugrouten` folder. """
+        files = []
+        for file in os.listdir(self.routes):
+            files.append(os.path.join(self.routes,file))
+
+        return files
+
+    def calculate_tank_stops(self, file_name):
         starting_fuel = 0.0
         dates = []
         gas_station_ids = []
+
+        # Read initial gas load
         with open(file_name, 'r') as f:
             for line in f:
                 line = line.replace('\n', '')
@@ -83,34 +96,109 @@ class InformatiCup2018(object):
                     dates.append(line.split(';')[0])
                     gas_station_ids.append(line.split(';')[1])
 
+        # Read route information
         # Time is the date of arrival at the gas station
         # Hash tag is the id of the gas station
         route = pd.read_csv(file_name, delimiter=';', skiprows=1, names=["Time", "#"])
-        capacity = starting_fuel
         self.init_gas_stations()
-        tanks = self.gas_stations[['Long','Lat']]
+
+        # predict prices for given datetimes and gas stations. This will take a while!
+        r = []
+        for index, row in route.iterrows():
+            self.read_gas_station_data(row['#'])
+            self.create_model_for_gas_station(row['#'])
+            a = self.predict_prices(row['#'],[row['Time']])
+            r.append(a[['yhat']].loc[0][0])
+            print(row['Time'], row['#'])
+
+        capacity = starting_fuel
+        tanks = self.gas_stations[['Long', 'Lat']]
 
         G = pd.DataFrame(columns=['V', 'E'])
 
-        print(starting_fuel)
-        print(dates)
-        print(gas_station_ids)
+        def station_dist(x=route['#'], y=tanks):
+            z = []
+            for i in range(len(x) - 1):
+                z.append(dist(y.loc[x.loc[i]], y.loc[x.loc[i + 1]]))
+            return [0] + z
 
+        # Edge
+        G['E'] = station_dist()
+        # Vertice(node)
+        G['V'] = r
+        G['V'] = G['V'].astype(int)
 
+        def cost2(G=G, cap=capacity):
+            id_and_price = pd.DataFrame()
+            # 0.056 l/km as mentioned in documentation
+            l = 0.056
+            fill = pd.Series(index=G.index, data=0)
+            fill.loc[0] = cap
+            actual_gas = cap
+            id_and_price['price'] = G['V'].loc[1:]
+            id_and_price['dist'] = G['E'].loc[1:]
+            # id_and_price['dist_in_liter'] = id_and_price['dist'].values.cumsum()
+            # id_and_price.loc[:, 'dist_in_liter'] *= l
+            id_and_price.sort_values(inplace=True, by='price')
+            j = len(id_and_price)
+            i = 0
+            # print(id_and_price)
+            while i < j:
+                g = id_and_price.index[i]
+                # print(id_and_price.index[i])
+                # 8ка это растояние между 7 и 8
+                # print(id_and_price['dist'].loc[id_and_price['dist'].index <= id_and_price.index[i]])
+                # we needed to fill 0.49 not cap
+                x = (sum(id_and_price['dist'].loc[id_and_price['dist'].index <= id_and_price.index[i]]) * l)
+                if x <= actual_gas:
+                    # if id_and_price.index[i] == len(G) - 1:
+                    # break
+                    actual_gas = actual_gas - x
+                    lst_to_drop = id_and_price.index[id_and_price.index <= id_and_price.index[i]]
+                    id_and_price.drop(labels=lst_to_drop, axis=0, inplace=True)
+                    # print(id_and_price)
+                    k = 0
+                    j = len(id_and_price)
+                    while k < j:
+                        x = (sum(id_and_price['dist'].loc[id_and_price['dist'].index <= id_and_price.index[k]]) * l)
+                        # print(id_and_price.index[k])
+                        if x <= cap:
+                            actual_gas = actual_gas + x
+                            fill.loc[g] = x
+                            print(fill)
+                            break
+                        else:
+                            k = k + 1
+                    i = k
+                    j = len(id_and_price)
+                else:
+                    i = i + 1
+            return fill
 
-        predicted_prices = []
-        for i, gas_station in enumerate(gas_station_ids):
-            self.read_gas_station_data(gas_station)
-            self.create_model_for_gas_station(gas_station)
-            predicted_prices.append(self.predict_prices(gas_station, [dates[i]]))
-            print(predicted_prices)
+        G['F'] = cost2()
+        route_result = route.copy()
+        route_result['Price'] = G['V']
+        route_result['Fuel'] = G['F']
+        route_result.to_csv('/Users/ole/Berta30.csv', sep=';', header=None, index=None, decimal=',')
 
-        print(predicted_prices)
-        self.write_predicted_prices_to_csv('/Users/ole/Berta3.csv',predicted_prices,gas_station_ids,starting_fuel)
+        # TODO: Add code from Algorithm notebook to finish processing
+
+        # print(starting_fuel)
+        # print(dates)
+        # print(gas_station_ids)
+        #
+        # predicted_prices = []
+        # for i, gas_station in enumerate(gas_station_ids):
+        #     self.read_gas_station_data(gas_station)
+        #     self.create_model_for_gas_station(gas_station)
+        #     predicted_prices.append(self.predict_prices(gas_station, [dates[i]]))
+        #     print(predicted_prices)
+        #
+        # print(predicted_prices)
+        # self.write_predicted_prices_to_csv('/Users/ole/Berta3.csv', predicted_prices, gas_station_ids, starting_fuel)
 
     def write_predicted_prices_to_csv(self, file, predicted_prices, gas_stations, starting_fuel):
         with open(file, 'w') as f:
-
             for i, gs in enumerate(gas_stations):
                 line = ''
                 line += str(predicted_prices[i]['ds'].get(0)) + ';'
@@ -123,8 +211,9 @@ class InformatiCup2018(object):
 if __name__ == '__main__':
     ic = InformatiCup2018(data_dir="/Users/ole/InformatiCup2018/")
     # data = ic.read_gas_station_data(1)
-
-    ic.predict_route_prices('/Users/ole/InformatiCup2018/Eingabedaten/Fahrzeugrouten/Bertha Benz Memorial Route.csv')
+    for f in ic.get_all_route_files():
+        ic.calculate_tank_stops(f)
+    # ic.predict_route_prices('/Users/ole/InformatiCup2018/Eingabedaten/Fahrzeugrouten/Bertha Benz Memorial Route.csv')
     # ic.predict_route_prices('/Users/ole/Berta2.csv')
     # predict_dates = list(map(str, data['ds'].tolist()[-5:]))
     # predict_dates = pd.date_range('9/21/2016', periods=365*24, freq='H')
